97a9f07c5965dc9c0d114b27bdffdd59f5372dcd
Gabriel Ferreira Pereira <gabrielbee1809@gmail.com>
2025-06-03 12:59:51 -0300

    Add files via upload

---
+++ VisualCode/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using LogicaComCSharp;
+
+
+// Exemplos:
+
+// ExemploFor.Main(args);
+
+// Temporizador.Main(args);
+
+// Cronometro.Main(args);
+
+// Manzano Páginas 25-26
+
+// ManzanoPagina25Ex7A.Main(args);
+
+// ManzanoPagina25Ex7B.Main(args);
+
+// ManzanoPagina25Ex7C.Main(args);
+
+// Manzano Página 66
+
+// ManzanoPagina66Ex1A.Main(args);
+
+// ManzanoPagina66Ex1B.Main(args);
+
+// ManzanoPagina66Ex1C.Main(args);
+
+// ManzanoPagina66Ex1D.Main(args);
+
+// ManzanoPagina66Ex1E.Main(args);
+
+// ManzanoPagina66Ex1F.Main(args);
